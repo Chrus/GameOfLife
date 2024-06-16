f9ef08a198871ef6973474703b5351a4e93c1f7d
Chrus <cdominessy@gmail.com>
2024-06-15 21:03:54 -0400

    Refactoring

--- Engine/Rect.cpp
+++ Engine/Rect.cpp
@@ -17,40 +17,6 @@ Rect::Rect(Tuple position, Tuple size)
 	size(size)
 {}
 
-bool Rect::overlaps(const Rect& other) const
-{
-	return  
-		Right() > other.Left() &&
-		Left() < other.Right() &&
-		Bottom() > other.Top() &&
-		Top() < other.Bottom();
-}
-bool Rect::contains(const Rect&  constother) const
-{
-	return
-		Left() <= other.Left() &&
-		Right() >= other.Right() &&
-		Top() <= other.Top() &&
-		Bottom() >= other.Bottom();
-}
-bool Rect::contains(const Tuple& point) const
-{
-	return
-		point.x >= Left() &&
-		point.x <= Right() &&
-		point.y >= Top() &&
-		point.y <= Bottom();
-}
-
-void Rect::setPosition(const Tuple& newPosition)
-{
-	position = newPosition;
-}
-void Rect::setPosition(const int x, const int y)
-{
-	position.x = x;
-	position.y = y;
-}
 void Rect::moveBy(const Tuple& amount)
 {
 	position += amount;
@@ -60,16 +26,6 @@ void Rect::moveBy(const int xAmount, const int yAmount)
 	position.x += xAmount;
 	position.y += yAmount;
 }
-
-void Rect::setSize(const Tuple& newSize)
-{
-	size = newSize;
-}
-void Rect::setSize(const int width, const int height)
-{
-	size.x = width;
-	size.y = height;
-}
 void Rect::reSize(const Tuple& amount)
 {
 	size += amount;
@@ -78,4 +34,34 @@ void Rect::reSize(const int widthAmount, const int heightAmount)
 {
 	size.x += widthAmount;
 	size.y += heightAmount;
+}
+
+Rect Rect::getExpanded(const int offset) const
+{
+	return Rect(left() - offset, top() - offset, width() + (offset * 2), height() + (offset * 2));
+}
+
+bool Rect::overlaps(const Rect& other) const
+{
+	return
+		right() > other.left() &&
+		left() < other.right() &&
+		bottom() > other.top() &&
+		top() < other.bottom();
+}
+bool Rect::contains(const Rect& other) const
+{
+	return
+		left() <= other.left() &&
+		right() >= other.right() &&
+		top() <= other.top() &&
+		bottom() >= other.bottom();
+}
+bool Rect::contains(const Tuple& point) const
+{
+	return
+		point.x >= left() &&
+		point.x <= right() &&
+		point.y >= top() &&
+		point.y <= bottom();
 }
--- Engine/Rect.h
+++ Engine/Rect.h
@@ -11,35 +11,28 @@ public:
 	Rect(int x, int y, int width, int height);
 	Rect(Tuple position, Tuple size);
 
-	//Getters
-	int Rect::X() const { return position.x; }
-	int Rect::Y() const { return position.y; }
-	int Rect::Width() const { return size.x; }
-	int Rect::Height() const { return size.y; }
-	int Rect::Left() const { return X(); }
-	int Rect::Right() const { return X() + Width(); }
-	int Rect::Top() const { return Y(); }
-	int Rect::Bottom() const { return Y() + Height(); }
-	Tuple Rect::Position() const { return position; }
-	Tuple Rect::Size() const { return size; }
+	//Qol Getters
+	int Rect::x() const { return position.x; }
+	int Rect::y() const { return position.y; }
+	int Rect::width() const { return size.x; }
+	int Rect::height() const { return size.y; }
+	int Rect::left() const { return position.x; }
+	int Rect::right() const { return position.x + size.x; }
+	int Rect::top() const { return position.y; }
+	int Rect::bottom() const { return position.y + size.y; }
 
-	//Setters (position)
-	void Rect::setPosition(const Tuple& newPosition);
-	void Rect::setPosition(const int x, const int y);
 	void Rect::moveBy(const Tuple& amount);
 	void Rect::moveBy(const int xAmount, const int yAmount);
-	//Setters (size)
-	void Rect::setSize(const Tuple& newSize);
-	void Rect::setSize(const int width, const int height);
 	void Rect::reSize(const Tuple& amount);
 	void Rect::reSize(const int widthAmount, const int heightAmount);
+
+	Rect getExpanded(const int offset) const;
 
 	//Comparisions
 	bool Rect::overlaps(const Rect& other) const;
 	bool Rect::contains(const Rect& other) const;
 	bool Rect::contains(const Tuple& point) const;
 
-private:
 	//Variables
 	Tuple position;
 	Tuple size;
