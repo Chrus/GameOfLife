908d919ccce85bf6eb44ab6f8296d16efc981a21
Chrus <cdominessy@gmail.com>
2024-07-02 17:46:35 -0400

    ExpandablePanel framework.  Just added the class, there is nothing in it yet

---
+++ Engine/ExpandablePanel.cpp
@@ -0,0 +1,2 @@
+#include "ExpandablePanel.h"
+
---
+++ Engine/ExpandablePanel.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Container.h"
+
+class ExpandablePanel: public Container
+{
+public:
+	ExpandablePanel(Rect header);
+
+protected:
+	//ActionPanel header;
+	//Container expandedPanel;
+};
+
